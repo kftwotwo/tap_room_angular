61475a05e5c5a16ac00fa732b21e4afc49d935cb
Kevin Finley <kevin@>
2017-01-18 15:01:36 -0800

    add bootstrap 4

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,15 +6,34 @@ import { Component } from '@angular/core';
   <div class="well">
     <h1 class="center">Here are all the taps</h1>
     <ul>
-     <li class="center" *ngFor="let currentKeg of kegs"><p><span class="som">{{currentKeg.nameDrink}}</span><br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}} <br>Pints: {{currentKeg.pints}}</p><button class="center btn btn-warning" (click)="editKeg(currentKeg)">Edit</button><hr></li>
+     <li class="center" *ngFor="let currentKeg of kegs"><p><span class="som">{{currentKeg.nameDrink}}</span><br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}} <br>Pints: {{currentKeg.pints}}</p><button class="center btn btn-outline-warning" (click)="editKeg(currentKeg)">Edit</button></li>
    </ul>
-     <div *ngIf="selectedKeg">
-        <h3>Edit</h3>
-        <label>Enter Task Description:</label>
-        <input [(ngModel)]="selectedKeg.nameDrink">
+   <div class="row">
+     <div *ngIf="selectedKeg" class="col-md-6 center-form">
+        <h3 class="center">Edit</h3>
+        <div class="input-group">
+          <label>Name</label>
+          <input class="form-control" [(ngModel)]="selectedKeg.nameDrink"><br>
+        </div>
+        <div class="input-group">
+          <label>Price</label>
+          <span class="input-group-addon">$</span>
+          <input class="form-control" [(ngModel)]="selectedKeg.price"><br>
+        </div>
+        <div class="input-group">
+          <label>Alcohol Content</label>
+          <input class="form-control" [(ngModel)]="selectedKeg.alcoholContent"><br>
+        </div>
+        <div class="input-group">
+        <label>Pints</label>
+        <input class="form-control" [(ngModel)]="selectedKeg.pints"><br>
+        </div>
         <br>
-        <button (click)="finishedEditing()">Save</button>
+        <div class="button-center">
+          <button class="center btn btn-outline-success" (click)="finishedEditing()">Save</button>
+        </div>
       </div>
+    </div>
    </div>
   `
 })
