fd5619d6bb6a92f78a4e9508da007be7b0165ddb
Kevin Finley <kevin@>
2017-01-18 10:03:55 -0800

    able to see all kegs on homepage

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>This is a import statement </h1>
-  </div>
+    <h1>Here are all the taps</h1>
+    <ul>
+     <li *ngFor="let currentKeg of kegs">{{currentKeg.nameDrink}} - {{currentKeg.price}} {{currentKeg.alcoholContent}} {{currentKeg.pints}}</li>
+   </ul>
   `
 })
 
 export class AppComponent {
+kegs: Keg[] = [
+    new Keg("Duff", "$150", "Simpsons", "Beer", "124 pints"),
+    new Keg("Colt 45", "$100", "IDK", "Beer", "120 pints"),
+    new Keg("Budlight", "$200", "budwiser", "Beer", "100 pints")
+  ];
+}
 
+export class Keg {
+  constructor(public nameDrink: string, public price: string, public brand: string, public alcoholContent: string, public pints: string){}
 }
--- app/keg.ts
+++ app/keg.ts
@@ -1,9 +1,3 @@
 class Keg {
   constructor(public nameDrink: string, public price: string, public brand: string, public alcoholContent: string, public pints: string){}
 }
-
-var kegs: Keg[] = [];
-kegs.push(new Keg("Duff", "$150", "Simpsons", "Beer", "124 pints"));
-kegs.push(new Keg("Colt 45", "$100", "IDK", "Beer", "120 pints"));
-kegs.push(new Keg("Budlight", "$200", "budwiser", "Beer", "100 pints"));
-console.log(kegs);
